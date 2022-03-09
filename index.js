40e1ab034d221d8e7cbf1c5825857680b35bd4eb
Kauani Santos <101286619+kakaushow1@users.noreply.github.com>
2022-03-09 18:56:11 +0000

    more one objeto(extra)

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ class Retangulo{
 
 let quadrado= new Retangulo("10","10");
 let retangulo= new Retangulo ("60", "30");
+let extra= new Retangulo ("10", "70");
 console.log(quadrado.mostrarAtributos(), retangulo.mostrarAtributos())
+console.log(extra.mostrarAtributos(), extra.mostrarAtributos())
 console.log(quadrado.calcularArea(), retangulo.calcularArea())
 console.log(quadrado.area(), retangulo.area())
